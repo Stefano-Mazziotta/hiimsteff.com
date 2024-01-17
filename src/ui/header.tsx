04ca91417f7d3db53f7ab6e80eb926e573a7e37f
smazziotta <mazziottastefano@gmail.com>
2024-01-16 23:05:01 -0300

    modify header

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -5,14 +5,11 @@ export default function Header() {
     <header>
       <nav className="p-4 flex justify-between items-center">
         <Link href="#">
-          <h1 className="text-4xl text-bold">SMZT</h1>
+          <h1 className="text-4xl font-semibold text-cyan-600">SMZT</h1>
         </Link>
         <ul className="flex gap-4">
           <li>
             <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/Albums">Albums</Link>
           </li>
         </ul>
       </nav>
