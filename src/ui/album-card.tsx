7da4273687140e6309df04e5c8330d4b5c14c2e0
smazziotta <mazziottastefano@gmail.com>
2024-01-18 13:01:21 -0300

    shared component - information label

--- src/ui/album-card.tsx
+++ src/ui/album-card.tsx
@@ -15,8 +15,10 @@ export default function AlbumCard({ album }: { album: album }) {
         height={700}
         className="absolute inset-0 h-full w-full object-cover"
       />
-      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/70 md:transition-opacity md:group-hover:opacity-70"></div>
-      <h2 className="z-10 mt-3 text-lg font-bold text-white">{album.title}</h2>
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/30 md:transition-opacity md:group-hover:opacity-70"></div>
+      <h2 className="w-fit z-10 inline-block py-1 px-2 mt-3 text-xs rounded font-semibold text-teal-200 bg-teal-600 last:mr-0 mr-1">
+        {album.title}
+      </h2>
     </Link>
   );
 }
