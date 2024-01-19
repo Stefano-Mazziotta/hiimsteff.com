f54880f53e1c42445a7479dfc93902ae65defe29
smazziotta <mazziottastefano@gmail.com>
2024-01-18 22:40:31 -0300

    album card - adjust styles

--- src/ui/album-card.tsx
+++ src/ui/album-card.tsx
@@ -6,7 +6,7 @@ export default function AlbumCard({ album }: { album: album }) {
   return (
     <Link
       href={`album/${album.slug}`}
-      className="group relative isolate flex flex-col justify-end overflow-hidden px-2 pb-2 pt-60 w-full h-max"
+      className="group relative isolate flex flex-col justify-end overflow-hidden px-2 pb-2 pt-56 w-full h-max"
     >
       <Image
         src={album.thumbnail.url}
@@ -16,7 +16,7 @@ export default function AlbumCard({ album }: { album: album }) {
         className="absolute inset-0 h-full w-full object-cover"
       />
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/30 md:transition-opacity md:group-hover:opacity-70"></div>
-      <h2 className="w-fit z-10 inline-block py-1 px-2 mt-3 text-xs rounded font-semibold text-teal-200 bg-teal-600 last:mr-0 mr-1">
+      <h2 className="w-fit z-10 inline-block py-1 px-2 mt-3 text-xl font-semibold text-cyan-50 bg-cyan-800 bg-opacity-60 md:transition-all md:group-hover:bg-opacity-100 text-shadow">
         {album.title}
       </h2>
     </Link>
