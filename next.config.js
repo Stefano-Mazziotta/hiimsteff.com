44f5010f7db8b35e81aaba97a5fefd18652f633a
stefano-mazziotta <mazziottastefano@gmail.com>
2025-02-13 16:16:32 -0300

    fix - production deployment - RangeError: Maximum call stack size exceeded

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "smzt-com.vercel.app",
+        hostname: "hiimsteff.com",
         port: "",
         pathname: "**",
       },
