9055c21cd2aececaa0cb881d00dfb2aaaa5a0009
stefano-mazziotta <mazziottastefano@gmail.com>
2025-02-14 02:33:15 -0300

    update json to real data

--- src/components/album/album-card.tsx
+++ src/components/album/album-card.tsx
@@ -13,8 +13,11 @@ export default async function AlbumCard({ album }: { album: Album }) {
   return (
     <Link
       href={`/gallery/${slug}`}
-      className="h-100 group relative isolate z-10 flex min-h-60 w-full flex-col justify-end"
+      className="h-100 group relative isolate z-10 flex min-h-60 w-full flex-col justify-start border-4 border-white"
     >
+      <h2 className="z-20 mb-2 ml-2 mt-2 inline-block w-fit bg-black bg-opacity-100 px-2 py-1 text-base font-semibold text-white">
+        {title}
+      </h2>
       <Image
         src={featuredPhoto}
         alt={title}
@@ -26,9 +29,6 @@ export default async function AlbumCard({ album }: { album: Album }) {
         priority
       />
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/30 md:transition-opacity md:group-hover:opacity-70"></div>
-      <h2 className="z-10 mb-2 ml-2 inline-block w-fit bg-black bg-opacity-100 px-2 py-1 text-base font-semibold text-white md:bg-opacity-60 md:transition-all md:group-hover:bg-opacity-100">
-        {title}
-      </h2>
     </Link>
   );
 }
