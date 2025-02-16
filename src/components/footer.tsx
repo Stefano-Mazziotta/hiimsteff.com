e0b27f62d3a9ba3a9b6b6fc1eeaeaffb8e5fee6c
stefano mazziotta <smazziotta@foodlus.com>
2025-02-15 21:25:12 -0300

    improve the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,15 +3,12 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="mt-5 flex justify-center border-t-2 border-cyan-900 p-4 md:mt-10">
-      <p>
-        Made with{" "}
-        <span role="img" aria-label="love">
-          ❤️‍🔥
-        </span>{" "}
-        by{" "}
+      <p className="text-sm text-center text-gray-500">
+        Photographs and website by{" "} 
         <Link
-          className="text-blue-600 visited:text-purple-600"
+          className="text-blue-600 visited:text-purple-600 hover:underline"
           href="https://www.github.com/stefano-mazziotta"
+          target="_blank"
         >
           Stefano Mazziotta
         </Link>
